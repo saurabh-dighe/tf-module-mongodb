4fff9f031a0f80a1383185f22ec8fd94014ba590
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-05 19:33:18 +0530

    auto push

--- docdb.tf
+++ docdb.tf
@@ -1,18 +1,22 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}-docdb"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "RoboShop1"
-  # backup_retention_period = 5                 Enable in prod
-  # preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-}
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "roboshop-${var.ENV}-docdb"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "RoboShop1"
+#   # backup_retention_period = 5                 Enable in prod
+#   # preferred_backup_window = "07:00-09:00"
+#   skip_final_snapshot     = true
+# }
 
-resource "aws_docdb_subnet_group" "docdb" {
-  name       = "roboshop-${var.ENV}-docdb"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID
+# resource "aws_docdb_subnet_group" "docdb" {
+#   name       = "roboshop-${var.ENV}-docdb"
+#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID
 
-  tags = {
-    Name = "roboshop-${var.ENV}-subent-grp"
-  }
+#   tags = {
+#     Name = "roboshop-${var.ENV}-subent-grp"
+#   }
+# }
+
+output "private_subnet" {
+  value = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID
 }
