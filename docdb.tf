028b7115fbd3522492366cdc6efdd8e2a4d5d6a3
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-06 14:19:08 +0530

    auto pushhh

--- docdb.tf
+++ docdb.tf
@@ -28,22 +28,22 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "allow_docdb" {
   name        = "roboshop-${var.ENV}-mongodb-sg"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "roboshop-${var.ENV}-mongodb-sg"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
     from_port        = 27017
     to_port          = 27017
     protocol         = "tcp"
-    cidr_blocks      = data.terraform_remote_state.vpc.outputs.VPC_CIDR
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
-  ingress {
-    from_port        = 27017
-    to_port          = 27017
-    protocol         = "tcp"
-    cidr_blocks      = data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   
   tags = {
