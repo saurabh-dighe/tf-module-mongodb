f02ba376aadc6430fb185cacf3911ac6b7698b55
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-06 12:30:40 +0530

    auto pushhh

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-statefile-saurabh"
-    key    = "dev/terraform-vpc/teraform.tfstate"
+    bucket = "saurabh-bucket-tf"
+    key    = "dev/tf-vpc/teraform.tfstate"
     region = "us-east-1"
   }
 }
