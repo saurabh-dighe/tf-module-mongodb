0d5bb4e9b3bbe954cc3f8738a920dbb3dda054c0
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-06 16:19:45 +0530

    auto pushhh

--- docdb.tf
+++ docdb.tf
@@ -7,7 +7,7 @@ resource "aws_docdb_cluster" "docdb" {
   # preferred_backup_window = "07:00-09:00"
   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name 
   skip_final_snapshot     = true
-  # vpc_security_group_ids  = [aws_security_group.allow_docdb.id]
+  vpc_security_group_ids  = [aws_security_group.allow_docdb.id]
 }
 
 resource "aws_docdb_subnet_group" "docdb" {
@@ -29,25 +29,3 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   }
 }
 
-resource "aws_security_group" "allow_docdb" {
-  name        = "roboshop-${var.ENV}-mongodb-sg"
-  description = "roboshop-${var.ENV}-mongodb-sg"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
-
-  ingress {
-    from_port        = 27017
-    to_port          = 27017
-    protocol         = "tcp"
-    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  
-  tags = {
-    Name = "roboshop-${var.ENV}-mongodb-sg"
-  }
-}
