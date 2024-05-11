0bc3488558e7504c7c3204040a98ada74ac9e177
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-11 21:40:34 +0530

    auto pushhh

--- vars.tf
+++ vars.tf
@@ -1,8 +1,6 @@
 variable "ENV" {}
 
 variable "DOCDB_ENGINE_VERSION" {}
-variable "DOCDB_USENAME" {}
-variable "DOCDB_PASSWORD" {}
 variable "DOCDB_INSTANCE_TYPE" {}
 variable "DOCDB_INSTANCE_COUNT" {}
 variable "DOCDB_PORT" {}
