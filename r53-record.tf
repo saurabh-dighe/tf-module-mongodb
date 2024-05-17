731dfb996db827a20e16192b57b93057eff74c98
saurabh-dighe <164217342+saurabh-dighe@users.noreply.github.com>
2024-05-17 15:20:36 +0530

    Update r53-record.tf

--- r53-record.tf
+++ r53-record.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "docdb" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "docdb-${var.ENV}"
+  name    = "mongodb-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
   records = [aws_docdb_cluster.docdb.endpoint]
