d1e609c18bb358031356fd52159a94eaf004f429
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-06-23 19:07:09 +0530

    Auto push

--- schema.tf
+++ schema.tf
@@ -6,8 +6,8 @@ resource "null_resource" "schema" {
         command = <<EOF
             cd /tmp
             wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-            unzip -o main.zip
+            curl -s -L -o /tmp/mangodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+            unzip -o mongodb.zip
             cd mongodb-main
             mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password RoboShop1 < catalogue.js 
             mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password RoboShop1 < users.js 
