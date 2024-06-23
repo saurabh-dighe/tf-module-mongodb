b22e43403a38433627d1943b2809b00731698a27
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-06-23 15:05:52 +0530

    Auto push

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 
-locals {
-  DOCDB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
-  DOCDB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
-}
+# locals {
+#   DOCDB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+#   DOCDB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
+# }
