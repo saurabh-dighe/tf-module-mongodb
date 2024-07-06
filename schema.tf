9c386479eb407e3ba3f8e89cef2044608c502293
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-07-06 14:59:12 +0530

    Auto push

--- schema.tf
+++ schema.tf
@@ -1,17 +1,17 @@
-# resource "null_resource" "schema" {
+resource "null_resource" "schema" {
 
-#     # This make sures that this null_resource will only be executed post the creation of the RDS only    
-#     depends_on = [aws_docdb_cluster.docdb ,aws_docdb_cluster_instance.cluster_instances]
-#       provisioner "local-exec" {
-#         command = <<EOF
-#             cd /tmp
-#             rm -rf global-bundle.pem
-#             wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-#             curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-#             unzip -o mongodb.zip
-#             cd mongodb-main
-#             mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password RoboShop1 < catalogue.js 
-#             mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password RoboShop1 < users.js 
-#         EOF
-#     }
-# }
+    # This make sures that this null_resource will only be executed post the creation of the RDS only    
+    depends_on = [aws_docdb_cluster.docdb ,aws_docdb_cluster_instance.cluster_instances]
+      provisioner "local-exec" {
+        command = <<EOF
+            cd /tmp
+            rm -rf global-bundle.pem
+            wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+            curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+            unzip -o mongodb.zip
+            cd mongodb-main
+            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password RoboShop1 < catalogue.js 
+            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username admin1 --password RoboShop1 < users.js 
+        EOF
+    }
+}
